74e0c7f9668032c917e9d1636081b20b8b59065f
iamgauraav <iamgauraav@gmail.com>
2022-03-03 21:20:18 +0530

    Adding UC7 Using Function to get work hours

--- EmpWage.sh
+++ EmpWage.sh
@@ -1,19 +1,17 @@
 #!/bin/bash -x
 
+#CONTANTS
 isPartTime=1
 isFullTime=2
 MAX_HRS_IN_MONTH=100
 EMP_RATE_PER_HR=20
 NUM_WORKING_DAYS=20
 
-#variables
+#VARIABLES
 totalEmpHr=0
 totalWorkingDays=0
 
-while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
-do
-	((totalWorkingDays++))
-	empCheck=$(($RANDOM%3))
+function getWorkHr() {
 	case $empCheck in
 		$isFullTime)
 			empHr=14
@@ -25,7 +23,14 @@ do
 			empHr=0
 			;;
 esac
-totalEmpHr=$(($totalEmpHr + $empHr))
+echo $empHr
+}
+
+while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+        ((totalWorkingDays++))
+	empHr="$( getWorkHr $((RANDOM%3)) )"
+	totalEmpHr=$(($totalEmpHr + $empHr))
 done
 
 totalSalary=$(($totalEmpHr + $EMP_RATE_PER_HR))
