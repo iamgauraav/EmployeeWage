390a55bb1d28e4d08a0e92f712689f23f72c06c3
iamgauraav <iamgauraav@gmail.com>
2022-03-03 14:52:14 +0530

    Added UC2 Calculating daily Employee wage

--- EmpWage.sh
+++ EmpWage.sh
@@ -3,9 +3,11 @@
 isPresent=1
 randomCheck=$(($RANDOM%2))
 
-if [ $randomCheck -eq $isPresent ]
+if [ $isPresent -eq $randomCheck ]
 then
-	echo "Employee is present: "
+	empRatePerHr=20
+	empHrs=8
+	salary=$(($empHrs * $empRatePerHr))
 else
-	echo "Employee is absent: "
+	salary=0
 fi
