83ebcdce3df0c5bc59c0d8e7626e65cb186a621b
iamgauraav <iamgauraav@gmail.com>
2022-03-03 20:06:07 +0530

    Adding UC6 total 100 working hours or 20 days reached

--- EmpWage.sh
+++ EmpWage.sh
@@ -2,12 +2,17 @@
 
 isPartTime=1
 isFullTime=2
-totalSalary=0
-empRatePerHr=20
-numWorkingDays=20
+MAX_HRS_IN_MONTH=100
+EMP_RATE_PER_HR=20
+NUM_WORKING_DAYS=20
 
-for (( day=1;day<=$numWorkingDays;day++ ))
+#variables
+totalEmpHr=0
+totalWorkingDays=0
+
+while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
+	((totalWorkingDays++))
 	empCheck=$(($RANDOM%3))
 	case $empCheck in
 		$isFullTime)
@@ -20,8 +25,7 @@ do
 			empHr=0
 			;;
 esac
-
-salary=$(($empHr * $empRatePerHr))				
-totalSalary=$(($totalSalary+$salary))
-
+totalEmpHr=$(($totalEmpHr + $empHr))
 done
+
+totalSalary=$(($totalEmpHr + $EMP_RATE_PER_HR))
