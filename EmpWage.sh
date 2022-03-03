b4e6d6c7d9734fc6280a2536204e63425ef29493
iamgauraav <iamgauraav@gmail.com>
2022-03-03 15:27:34 +0530

    Adding UC5 Employee Wages for a month

--- EmpWage.sh
+++ EmpWage.sh
@@ -2,18 +2,26 @@
 
 isPartTime=1
 isFullTime=2
+totalSalary=0
 empRatePerHr=20
-randomCheck=$(($RANDOM%3))
+numWorkingDays=20
 
-case $randomCheck in
-	$isFullTime)
-		empHr=14
-		;;
-	$isPartTime)
-		empHr=8
-		;;
-	*)
-		empHr=0
-		;;
+for (( day=1;day<=$numWorkingDays;day++ ))
+do
+	empCheck=$(($RANDOM%3))
+	case $empCheck in
+		$isFullTime)
+			empHr=14
+			;;
+		$isPartTime)
+			empHr=8
+			;;
+		*)
+			empHr=0
+			;;
 esac
-salary=$(($empHr * $empRatePerHr))
+
+salary=$(($empHr * $empRatePerHr))				
+totalSalary=$(($totalSalary+$salary))
+
+done
